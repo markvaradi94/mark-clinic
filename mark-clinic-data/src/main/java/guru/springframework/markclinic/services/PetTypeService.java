ab12b4c16ec07b6fe5a6d5c4396380e3126634e2
Varadi <mark.varadi03@gmail.com>
2020-04-23 22:54:22 +0300

    added PetTypeService. Closes #29

---
+++ mark-clinic-data/src/main/java/guru/springframework/markclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package guru.springframework.markclinic.services;
+
+import guru.springframework.markclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ mark-clinic-data/src/main/java/guru/springframework/markclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,35 @@
+package guru.springframework.markclinic.services.map;
+
+import guru.springframework.markclinic.model.PetType;
+import guru.springframework.markclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        super.delete(petType);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return super.save(petType);
+    }
+}
