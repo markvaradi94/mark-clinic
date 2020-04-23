518516a210273ae73020aba88048f315224f564f
Varadi <mark.varadi03@gmail.com>
2020-04-23 11:09:57 +0300

    Implemented Map based Services. Closes %6

---
+++ mark-clinic-data/src/main/java/guru/springframework/markclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,34 @@
+package guru.springframework.markclinic.services.map;
+
+import guru.springframework.markclinic.model.Owner;
+import guru.springframework.markclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner owner) {
+        super.delete(owner);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        return super.save(owner.getId(), owner);
+    }
+}
