2645587e1a2dc1d20b95246f8a5c1a3b7b76e9e3
Varadi <mark.varadi03@gmail.com>
2020-04-23 23:02:12 +0300

    Added PetTypes to Bootstrap. Closes #32

--- mark-clinic-web/src/main/java/guru/springframework/markclinic/bootstrap/DataLoader.java
+++ mark-clinic-web/src/main/java/guru/springframework/markclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package guru.springframework.markclinic.bootstrap;
 
 import guru.springframework.markclinic.model.Owner;
+import guru.springframework.markclinic.model.PetType;
 import guru.springframework.markclinic.model.Vet;
 import guru.springframework.markclinic.services.OwnerService;
+import guru.springframework.markclinic.services.PetTypeService;
 import guru.springframework.markclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
