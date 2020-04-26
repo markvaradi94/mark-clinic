1d02992939276e9b9fc33774c443128ed40aad23
Varadi <mark.varadi03@gmail.com>
2020-04-26 14:19:03 +0300

    Added Speciality Map Impl. Close #31

---
+++ mark-clinic-data/src/main/java/guru/springframework/markclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package guru.springframework.markclinic.services;
+
+import guru.springframework.markclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
