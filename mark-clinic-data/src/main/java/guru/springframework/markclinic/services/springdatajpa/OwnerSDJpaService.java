101217191b3acfd59e963144a8366afedeff5395
Varadi <mark.varadi03@gmail.com>
2020-04-28 21:57:18 +0300

    Added JPA service for Speciality. Closes #44

--- mark-clinic-data/src/main/java/guru/springframework/markclinic/services/springdatajpa/OwnerSDJpaService.java
+++ mark-clinic-data/src/main/java/guru/springframework/markclinic/services/springdatajpa/OwnerSDJpaService.java
@@ -2,8 +2,6 @@ package guru.springframework.markclinic.services.springdatajpa;
 
 import guru.springframework.markclinic.model.Owner;
 import guru.springframework.markclinic.repositories.OwnerRepository;
-import guru.springframework.markclinic.repositories.PetRepository;
-import guru.springframework.markclinic.repositories.PetTypeRepository;
 import guru.springframework.markclinic.services.OwnerService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
@@ -16,14 +14,9 @@ import java.util.Set;
 public class OwnerSDJpaService implements OwnerService {
 
     private final OwnerRepository ownerRepository;
-    private final PetRepository petRepository;
-    private final PetTypeRepository petTypeRepository;
 
-    public OwnerSDJpaService(OwnerRepository ownerRepository, PetRepository petRepository,
-                             PetTypeRepository petTypeRepository) {
+    public OwnerSDJpaService(OwnerRepository ownerRepository) {
         this.ownerRepository = ownerRepository;
-        this.petRepository = petRepository;
-        this.petTypeRepository = petTypeRepository;
     }
 
     @Override
