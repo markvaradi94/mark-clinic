0ada299bbfd3482da9ef5b7b5de2331b396009e4
Varadi <mark.varadi03@gmail.com>
2020-05-03 18:15:28 +0300

    JUnit 5 upgrade. Closes #4

---
+++ mark-clinic-data/src/test/java/guru/springframework/markclinic/model/OwnerTest.java
@@ -0,0 +1,15 @@
+package guru.springframework.markclinic.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class OwnerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAddress() {
+    }
+}
