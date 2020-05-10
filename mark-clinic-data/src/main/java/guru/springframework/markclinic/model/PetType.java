ccba6387eaceb31aad68a784eb56fa61bef54e3c
Varadi <mark.varadi03@gmail.com>
2020-05-10 22:17:51 +0300

    Added ability to add or update pet. Closes # 56

--- mark-clinic-data/src/main/java/guru/springframework/markclinic/model/PetType.java
+++ mark-clinic-data/src/main/java/guru/springframework/markclinic/model/PetType.java
@@ -10,10 +10,15 @@ import javax.persistence.Table;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "types")
 public class PetType extends BaseEntity {
+
+    @Builder
+    public PetType(Long id, String name) {
+        super(id);
+        this.name = name;
+    }
 
     @Column(name = "name")
     private String name;
@@ -22,4 +27,6 @@ public class PetType extends BaseEntity {
     public String toString() {
         return name;
     }
+
+
 }
