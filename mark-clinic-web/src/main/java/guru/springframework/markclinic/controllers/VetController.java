8bbee24068a3fff8f79d133aa627a4954c3d4cc4
Varadi <mark.varadi03@gmail.com>
2020-05-16 14:38:50 +0300

    vets json endpoint. closes #66

--- mark-clinic-web/src/main/java/guru/springframework/markclinic/controllers/VetController.java
+++ mark-clinic-web/src/main/java/guru/springframework/markclinic/controllers/VetController.java
@@ -1,9 +1,13 @@
 package guru.springframework.markclinic.controllers;
 
+import guru.springframework.markclinic.model.Vet;
 import guru.springframework.markclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -18,5 +22,11 @@ public class VetController {
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @RequestMapping("/api/vets")
+    @ResponseBody
+    public Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
